905541ca95dad42e2e1833de587a95a8ce75038c
Drovosek01 <drovosek01tnt@gmail.com>
2024-08-11 18:37:28 +0300

    Fixed start cmd wrapper for parser template

--- wrappers/data in template/Start.cmd
+++ wrappers/data in template/Start.cmd
@@ -19,12 +19,12 @@ rem =====
 set "temp_filename_uniq="
 
 set "parser_name=Parser.ps1"
-set "parser_url_if_need=https://gist.github.com/Drovosek01/9d47068365ea0bce26526ee61b23be7c/raw/581aa5312b0f99745ca55c2646d31990a93e2ee3/ReplaceHexBytesAll.ps1"
-set "parser_path="
+set "parser_url_if_need=https://gist.aga.com/Drovosek01/9d47068365ea0bce26526ee61b23be7c/raw/581aa5312b0f99745ca55c2646d31990a93e2ee3/ReplaceHexBytesAll.ps1"
+set "parser_path= WRITE FULL PATH HERE !!!!!!"
 
 set "template_name=template.txt"
-set "template_url_if_need=https://gist.github.com/Drovosek01/9d47068365ea0bce26526ee61b23be7c/raw/581aa5312b0f99745ca55c2646d31990a93e2ee3/ReplaceHexBytesAll.ps1"
-set "template_path="
+set "template_url_if_need=https://gist.aga.com/Drovosek01/9d47068365ea0bce26526ee61b23be7c/raw/581aa5312b0f99745ca55c2646d31990a93e2ee3/ReplaceHexBytesAll.ps1"
+set "template_path=WRITE FULL PATH HERE !!!!!!"
 
 
 
@@ -75,21 +75,30 @@ rem =====
     exit /b
 
 
+:get_temp_filename_uniq
+    rem Get a unique full file name in the temporary folder
+    set "temp_filename_uniq=%temp%\patcher-%random%.ps1"
+    if exist %temp_filename_uniq% goto :get_temp_filename_uniq
+    exit /b
+
+
 :check_or_get_parser
     rem If the powershell script file is in the folder with the current script, use it
     rem otherwise download the script and use the downloaded one
-    if exist ".\%patcher_name%" (
-        call :set_filename ".\%patcher_name%"
-        set "patcher_path=!file!"
+    if defined parser_path (
         exit /b
-    ) else if exist "..\..\core\%patcher_name%" (
+    ) else if exist ".\%parser_name%" (
+        call :set_filename ".\%parser_name%"
+        set "parser_path=!file!"
+        exit /b
+    ) else if exist "..\..\core\%parser_name%" (
         rem patcher path in repository
-        call :set_filename "..\..\core\%patcher_name%"
-        set "patcher_path=!file!"
+        call :set_filename "..\..\core\%parser_name%"
+        set "parser_path=!file!"
         exit /b
     ) else (
         call :get_temp_filename_uniq
-        set "patcher_path=!temp_filename_uniq!"
+        set "parser_path=!temp_filename_uniq!"
         powershell -ExecutionPolicy Bypass -Command "(New-Object System.Net.WebClient).DownloadFile('%parser_url_if_need%','!parser_path!')"
     )
     exit /b
@@ -98,7 +107,9 @@ rem =====
 :check_or_get_template
     rem If the template.txt file is in the folder with the current script, use it
     rem otherwise download the script and use the downloaded one
-    if exist ".\%template_name%" (
+    if defined template_path (
+        exit /b
+    ) else if exist ".\%template_name%" (
         call :set_filename ".\%template_name%"
         set "template_path=!file!"
         exit /b
@@ -117,5 +128,5 @@ rem =====
 
 :parse_template
     rem Apply parser script and transfer template to it
-    powershell -NoLogo -NoProfile -ExecutionPolicy Bypass -Command "& %parser_path% -templatePath \"!template_path!\""
+    powershell -noexit -NoLogo -NoProfile -ExecutionPolicy Bypass -File "%parser_path%" "-templatePath" "!template_path!"
     exit /b
