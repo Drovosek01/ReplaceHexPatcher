1248d1b0e58a346868fb456d7846898dd43490c9
Drovosek01 <drovosek01tnt@gmail.com>
2024-08-22 21:34:59 +0300

    Moved required functions in separated scripts

--- wrappers/data in template/DeleteFilesOrFolders.ps1
+++ wrappers/data in template/DeleteFilesOrFolders.ps1
@@ -38,6 +38,87 @@ function DoWeHaveAdministratorPrivileges {
 
 <#
 .SYNOPSIS
+Function for check if for re-write transferred file need admins privileges
+
+.DESCRIPTION
+First, we check the presence of the "read-only" attribute and try to remove this attribute.
+If it is cleaned without errors, then admin rights are not needed (or they have already been issued to this script).
+If there is no "read-only" attribute, then we check the possibility to change the file.
+#>
+function Test-ReadOnlyAndWriteAccess {
+    [OutputType([bool[]])]
+    param (
+        [Parameter(Mandatory)]
+        [string]$targetPath,
+        [Parameter(Mandatory)]
+        [bool]$targetIsFile
+    )
+    
+    $fileAttributes = Get-Item -Path $targetPath | Select-Object -ExpandProperty Attributes
+    [bool]$isReadOnly = $false
+    [bool]$needRunAs = $false
+
+    if ($targetIsFile -and ($fileAttributes -band [System.IO.FileAttributes]::ReadOnly)) {
+        # if it file check "readonly" attribute
+        # folders in Windows have no "readonly" attribute and if target is folder - skip this check
+        try {
+            Set-ItemProperty -Path $targetPath -Name Attributes -Value ($fileAttributes -bxor [System.IO.FileAttributes]::ReadOnly)
+            $isReadOnly = $true
+            Set-ItemProperty -Path $targetPath -Name Attributes -Value ($fileAttributes -bor [System.IO.FileAttributes]::ReadOnly)
+            $needRunAs = $false
+        }
+        catch {
+            $isReadOnly = $true
+            $needRunAs = $true
+        }
+    } else {
+        $isReadOnly = $false
+
+        if ($targetIsFile) {
+            # if it file
+            # we check permissions for write and open - it mean we can modify file
+            try {
+                $stream = [System.IO.File]::Open($targetPath, [System.IO.FileMode]::Open, [System.IO.FileAccess]::Write)
+                $stream.Close()
+                $needRunAs = $false
+            } catch {
+                $needRunAs = $true
+            }
+        } else {
+            # if it folder
+            # we check permissions for delete folder
+            try {
+                # Here we need to check if we need administrator rights to manipulate the folder
+                # The only manipulation of the folder from the text in the template is to delete the folder
+                # I have not found a normal way to check if administrator rights are needed to delete a folder
+                # I found only an alternative way - to create a file in a folder and delete it.
+                #   If this happens without errors, then we do not need administrator rights to create and delete a file inside the folder.
+                #   Which means most likely to delete the folder too
+                # 
+                # But this is a bad way because creating and deleting a file is probably a more time-consuming procedure than checking the rights or attributes of a folder.
+                # Also, it does not check the actual right to delete the folder. Folders probably have many different rights and "access levels"
+                #   and if we have the ability/right to create + delete a file inside a folder,
+                #   then it's not a fact that we have the right to delete a folder (this is just my hypothesis)
+                # 
+                # TODO: Find a normal way to check if you need administrator rights to delete a folder
+                $tempFile = [System.IO.Path]::Combine($targetPath, [System.IO.Path]::GetRandomFileName())
+                [void](New-Item -Path $tempFile -ItemType File -Force -ErrorAction Stop)
+                Remove-Item -Path $tempFile -Force -ErrorAction Stop
+
+                $needRunAs = $false
+            }
+            catch {
+                $needRunAs = $true
+            }
+        }
+    }
+
+    return $isReadOnly, $needRunAs
+}
+
+
+<#
+.SYNOPSIS
 Move item (file or folder) to bin
 #>
 function Move-ToRecycleBin {
