bf914c1b8a9b99a6372496d75be252155e25cf3e
Drovosek01 <drovosek01tnt@gmail.com>
2024-08-22 21:38:28 +0300

    Moved required functions in separated scripts

--- wrappers/data in template/DetectFilesAndPatternsAndPatch.ps1
+++ wrappers/data in template/DetectFilesAndPatternsAndPatch.ps1
@@ -49,6 +49,43 @@ function DoesStringContainsOneItemArray {
 
 <#
 .DESCRIPTION
+Function get path to ps1 file, patch target-file and patterns array etc
+and run receive ps1 file and pass given arguments to ran script
+#>
+function RunPSFile {
+    param (
+        [Parameter(Mandatory)]
+        [string]$psFile,
+        [Parameter(Mandatory)]
+        [string]$targetFile,
+        [Parameter(Mandatory)]
+        [string]$patterns,
+        [Parameter(Mandatory)]
+        [bool]$makeBackup
+    )
+
+    [string]$patternsCleaned = $patterns -replace ",`"$",""
+
+    # The only .ps1 file that needs to be run from template is the patcher (main/core file)
+    # Previously there was additional code here to run the process as administrator or as usual, depending on different conditions
+    # But the logic of restarting on behalf of the administrator has been added to the script patcher.
+    # It looks like it makes no sense to repeat the logic of checking startup as an administrator (but this is not accurate),
+    #   but if necessary, run others.ps1 files, then you will need to return the logic of the conditions to run as administrator
+
+    if ($makeBackup) {
+        $process = Start-Process $PSHost -PassThru -Wait -NoNewWindow -ArgumentList "-ExecutionPolicy Bypass -File `"$psFile`" -filePath `"$targetFile`" -patterns", "$patternsCleaned", "-makeBackup"
+    } else {
+        $process = Start-Process $PSHost -PassThru -Wait -NoNewWindow -ArgumentList "-ExecutionPolicy Bypass -File `"$psFile`" -filePath `"$targetFile`" -patterns", "$patternsCleaned"
+    }
+
+    if ($process.ExitCode -gt 0) {
+        throw "Something happened wrong when patching file $targetFile"
+    }
+}
+
+
+<#
+.DESCRIPTION
 Function gets a text with lines loop all lines
 if detect valid path then all next lines concat like patterns until detect other path or end text
 then form path to target file and search/replace patterns and send it to function with run patcher script
